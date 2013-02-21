d596745d230fdaddb869da82127154f7e6b0545d
Rory McCann <rory@technomancy.org>
2013-02-21 17:59:02 +0000

    rename

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name="django-cache-properties",
       version="0.1",
       author="Rory McCann",
       author_email="rory@technomancy.org",
-      py_modules=['django_cache_property'],
+      py_modules=['django_cache_properties'],
       license = 'GPLv3',
       description = 'Prevent expensive computations by storing low level calues in the django cache',
 )
